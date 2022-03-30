1ec2d2e992c9bdebc864b93c0618b341ed2ece62
Lucas Rutowski <lucas.rutowski@gmail.com>
2022-03-30 22:07:03 +0000

    azurerm resource group

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/lrutowsk/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "kitty_test"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
